a7447ab2c34d78a86adab2f468660e91fc347348
Ivar1999 <249314@stud.inn.no>
2021-03-12 13:42:09 +0100

    Initial commit

---
+++ Source/GroupProject/GroupProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GroupProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GroupProject, "GroupProject" );
