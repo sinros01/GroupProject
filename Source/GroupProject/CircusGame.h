e64ba79efd0148b2de7d29610b8dab87a70b84fc
Ivar1999 <249314@stud.inn.no>
2021-03-12 14:39:22 +0100

    Level design and characterclass

---
+++ Source/GroupProject/CircusGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CircusGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUPPROJECT_API ACircusGame : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
