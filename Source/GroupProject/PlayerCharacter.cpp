de25741c4634f53f6d6530b424b25b8e09e2bc08
Ivar1999 <249314@stud.inn.no>
2021-03-12 16:00:11 +0100

    Camera setup, character and level changes

--- Source/GroupProject/PlayerCharacter.cpp
+++ Source/GroupProject/PlayerCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "PlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Camera/CameraActor.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -9,13 +12,32 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Camera setup
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring arm comp"));
+	SpringArm->SetupAttachment(RootComponent);
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera comp"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+}
+
+void APlayerCharacter::WalkForward(float Value)
+{
+	FVector Direction = GetActorForwardVector();
+
+	AddMovementInput(Direction, Value);
+}
+
+void APlayerCharacter::WalkRight(float Value)
+{
+	FVector Direction = GetActorRightVector();
+
+	AddMovementInput(Direction, Value);
 }
 
 // Called every frame
@@ -29,6 +51,10 @@ void APlayerCharacter::Tick(float DeltaTime)
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	// Walking bindings
+	PlayerInputComponent->BindAxis("WalkForward", this, &APlayerCharacter::WalkForward);
+	PlayerInputComponent->BindAxis("WalkRight", this, &APlayerCharacter::WalkRight);
+	// Character function jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
--- Source/GroupProject/PlayerCharacter.h
+++ Source/GroupProject/PlayerCharacter.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -14,16 +16,27 @@ class GROUPPROJECT_API APlayerCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	//Input functions
+	void WalkForward(float Value);
+	void WalkRight(float Value);
+
+
+	//Setting up camera
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		USpringArmComponent* SpringArm = nullptr;
+	UPROPERTY(EditAnywhere, Category = "Settings")
+		UCameraComponent* Camera = nullptr;
+
 
 };
